79cc6fe8e8b3e6a2f07f92ec9e6e4eb11ab1ab61
RLT <inco.k.b.blizz@gmail.com>
2026-03-10 14:47:48 +0300

    feat: added support for a second monitor and minor changes to nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+
+-- Перемещение строк вверх/вниз
+local map = vim.keymap.set
+
+-- Normal mode: переместить строку
+map("n", "<C-j>", ":m .+1<CR>==", { noremap = true, silent = true, desc = "Move line down" })
+map("n", "<C-k>", ":m .-2<CR>==", { noremap = true, silent = true, desc = "Move line up" })
+
+-- Visual mode: переместить выделенные строки
+map("v", "<C-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true, desc = "Move selection down" })
+map("v", "<C-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true, desc = "Move selection up" })
